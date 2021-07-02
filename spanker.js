48d354fe1ebd981fdfa5c6cb1cdb0134f75dcc3e
Spxnker <75320414+Spxnker@users.noreply.github.com>
2021-07-02 15:24:21 +0300

    Update spanker.js

--- spanker.js
+++ spanker.js
@@ -1,6 +1,6 @@
-Selam Canlarım 60 Star Gelene Kadar Paylaşmayı Düşünmüyorum Çünkü Çok Emek Verdiğim Bir Altyapı Oldu Teşekkür Ederim.
+Dostlar Normalde 50 Star Demiştim Fakat Çok Hızlı Olduğu İçin Kotayı Biraz arttıralım 70 Yapalım.
 
-60 Star Gelirse Paylaşılacağı Tarih 10.07.2021 dir. Eğer gelmezse süreyi birazdaha uzatırız. <3 
+70 Star Olduğu An Projeyi Size Sunacağım.
 
 Bu Projeden Haberdar Olmak İçin Beni Github Hesabımdan Takip Edebilirsin.
 
