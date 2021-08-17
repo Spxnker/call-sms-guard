7d514ae49d1f8be6b94f002e78fcb1737cae14bd
Spxnker <75320414+Spxnker@users.noreply.github.com>
2021-08-17 13:30:54 +0300

    Update ready.js

--- spanker log bot/events/ready.js
+++ spanker log bot/events/ready.js
@@ -4,7 +4,15 @@ const spxnker = require('../config/configuration.json');
 module.exports = client => {
   client.user.setStatus("dnd");
   setInterval(() => {
-      const spankme = ["Spanker 💙 Shinoa", "Spanker 💙 Miaf"];
+      const spankme = [
+        "Spanker 💙 Shinoa",
+       "Spanker 💙 Miaf",
+       "Spanker 💙 Null",
+       "artırabilir siniz.",
+       "",
+       "",
+       ""
+      ];
       const allahu = Math.floor(Math.random() * (spankme.length));
       client.user.setActivity(`${spankme[allahu]}`, {type: "PLAYING"});
     }, 10000);
