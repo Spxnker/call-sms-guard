4e5f7759fb21ac81081d1dfbdc8448d8987e21b5
Spxnker <75320414+Spxnker@users.noreply.github.com>
2021-08-17 09:31:44 +0300

    Update spanker.js

--- spanker.js
+++ spanker.js
@@ -1,8 +1,1 @@
-
-Çok uğraşılmış bir proje olduğu için 
-30 fork 70 star olunca paylaşma kararı aldim starin yanimda fork diyr bir özellik var
-
-Iyi günler 
-
-Bu Projeden Haberdar Olmak İçin Beni Github Hesabımdan Takip Edebilirsin.
-
+Hazırlıyorum 
