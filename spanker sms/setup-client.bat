4e0dac0093e7908cbc5b7c2e123ec04cbd859174
Spxnker <75320414+Spxnker@users.noreply.github.com>
2021-08-20 17:23:57 +0300

    Add files via upload

---
+++ spanker sms/setup-client.bat
@@ -0,0 +1,5 @@
+echo off
+cls
+npm i
+cls
+
---
+++ spanker sms/spank-me.js
@@ -0,0 +1,38 @@
+const Vonage = require('@vonage/server-sdk');
+
+// Çok istediğiniz Sms botunu paylaşıyorum.
+const { Bu, Bot, spanker, Tarafından, Yapılmıştır, } = require("discord.js")
+const Discord = require("discord.js") 
+const client = (global.client = new Discord.Client());
+const config = require('./config-client.json');
+// Spanker Ve Null Tarafın 'dan Geliştirildi.
+
+const spankerApi = new Vonage({ 
+  apiKey: config.simple.NEXMO_API_KEY, 
+  apiSecret: config.simple.NEXMO_API_SECRET
+}); 
+
+client.on("ready", async () => {
+  client.user.setPresence({ activity: { name: "Spanker Null Sms Log." }, status: "dnd" });
+  });
+
+
+client.on('roleDelete', async role => {
+  let entry = await role.guild.fetchAuditLogs({type: 'ROLE_DELETE'}).then(audit => audit.entries.first());
+  let spankerLogMessaj = `${entry.exexecutor} ${entry.executor.id} Tarafından \n${role.name} ${role.id} Rolleri Silindi!` 
+  spankerApi.message.sendSms(config.simple.VirtualNumber, config.simple.PhoneNumber, spankerLogMessaj, (err, responseData) => {
+  if (err) { console.log(err);
+    } else {
+      if (responseData.messages[0]['status'] === "0") { console.log(`${config.simple.PhoneNumber} Numarasına Mesaj Başarıyla Gönderildi.`);
+      } else {
+        console.log(`Mesaj gönderilirken bir hata ile karşılaşıldı: ${responseData.messages[0]['error-text']}`);
+      }
+    }
+  })
+
+});
+
+client.login(config.object.botToken)
+.then(c => console.log(`${client.user.tag} İsminde Giriş Gerçekleşti!`))
+.catch(err => console.error("Bot Başlatılamadı!"
+));
