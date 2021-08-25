d6a25490fdc391ef1bc131ffa08b5fb3350732a4
Spxnker <75320414+Spxnker@users.noreply.github.com>
2021-08-25 17:13:17 +0300

    Update spank-me.js

--- spanker guard/spank-me.js
+++ spanker guard/spank-me.js
@@ -1,3 +1,2 @@
-// HAZIRLANINCA SUNACAĞIM TAHMİNİ YAYINLANMA SÜRESİ 25.08.2021
-
-// Yayınlanana Kadar Log Ve Sms Botunu inceleyebilirsiniz.
+Guard ile sms botunu ve log botunu birleştirip sunacağım neden gecikti demeyin telefon arama güvenlik sistemini 'de güzel bir star sayısı gelirse yaparım 
+birde 2 tane guard sunucam 1.si owner guard 2.si normal guard
