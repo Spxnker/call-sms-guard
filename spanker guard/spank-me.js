f7fc2e88daab519e763fc42460ef260e84aa2738
Spxnker <75320414+Spxnker@users.noreply.github.com>
2021-08-30 12:34:00 +0300

    Delete spank-me.js

--- spanker guard/spank-me.js
+++
@@ -1,2 +0,0 @@
-Guard ile sms botunu ve log botunu birleştirip sunacağım neden gecikti demeyin telefon arama güvenlik sistemini 'de güzel bir star sayısı gelirse yaparım 
-birde 2 tane guard sunucam 1.si owner guard 2.si normal guard
